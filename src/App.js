0cfe14eeb5832180012acff1dfe2a99e660b44e2
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2021-10-06 14:10:23 +0530

    Initializing custom chat feed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
                 projectID="69560b1c-5b18-41ce-8e22-01d97414fb27"
                 userName="Kosu"
                 userSecret="asdfghjkl1"
+                renderChatFeed={(chatProps) => <ChatFeed {...chatProps}/>}
             />
         </>
     )
